624c390eba6b7c0924a9bb3736a273ace0268783
Ra'Jiska <dodo.lasticot@gmail.com>
2019-02-04 11:10:13 +0100

    feat(server): MariaDB Setup

--- server/html/pages/about.php
+++ server/html/pages/about.php
@@ -1,11 +1,7 @@
 <?php
-/* Client */
 $data = [
 	"client" => ["host" => $_SERVER['REMOTE_ADDR']],
 	"server" => ["current_time" => time(), "services" => []]
 ];
 
 echo json_encode($data, JSON_PRETTY_PRINT);
-//echo json_encode(["hello", "world"]);
-
-/* Server */
