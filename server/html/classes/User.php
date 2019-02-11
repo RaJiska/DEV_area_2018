e6a6e6753f41b220ecd6b06131aafedff1e01818
Unknown <quentin.santos@epitech.eu>
2019-02-11 22:31:13 +0100

    feat(access_token): add access token generation at registration

--- server/html/classes/User.php
+++ server/html/classes/User.php
@@ -3,49 +3,68 @@ require_once 'classes/DatabaseObject.php';
 
 class User extends DatabaseObject
 {
-        /* DB Fields */
+	/* DB Fields */
 	protected $field_login;
 	protected $field_pass;
+	protected $field_token;
 	protected $field_enabled = 1;
 
-        function loadById($id) /* throw */
-        {
-                $stmt = $this->Database->prepare("SELECT * FROM users WHERE id = ?;");
-                $stmt->execute([$id]);
-                if (!$stmt->rowCount())
-                        throw new Exception("User with ID " . $id . " does not exist");
-                $res = $stmt->fetch();
-                $this->field_id = $res['id'];
-                $this->field_login = $res['login'];
-                $this->field_pass = $res['pass'];
-                $this->field_enabled = $res['enabled'];
-        }
+	function loadByLogin($login)
+	{
+		$stmt = $this->Database->prepare("SELECT * FROM users WHERE login = ?;");
+		$stmt->execute([$login]);
+		if (!$stmt->rowCount())
+			throw new Exception("User with login " . $login . " does not exist");
+		$res = $stmt->fetch();
+		$this->field_id = $res['id'];
+		$this->field_login = $res['login'];
+		$this->field_pass = $res['pass'];
+		$this->field_token = $res['token'];
+		$this->field_enabled = $res['enabled'];
+	}
 
-        function insert() /* throw */
-        {
-                $this->insertQuery("INSERT INTO users (login, pass, enabled) VALUES (?, ?, 1);", array(
-                        $this->login,
-                        $this->pass
-                ));
-        }
+	function loadById($id) /* throw */
+	{
+		$stmt = $this->Database->prepare("SELECT * FROM users WHERE id = ?;");
+		$stmt->execute([$id]);
+		if (!$stmt->rowCount())
+			throw new Exception("User with ID " . $id . " does not exist");
+		$res = $stmt->fetch();
+		$this->field_id = $res['id'];
+		$this->field_login = $res['login'];
+		$this->field_pass = $res['pass'];
+		$this->field_token = $res['token'];
+		$this->field_enabled = $res['enabled'];
+	}
 
-        function update() /* throw */
-        {
-                $this->updateQuery("UPDATE users SET login = ?, pass = ?, enabled = ? WHERE id = ?;", array(
-                        $this->login,
-                        $this->pass,
-                        $this->enabled,
-                        $this->id
-                ));
-        }
+	function insert() /* throw */
+	{
+		$this->field_token = bin2hex(random_bytes(32 / 2));
+		$this->insertQuery("INSERT INTO users (login, pass, token, enabled) VALUES (?, ?, ?, 1);", array(
+			$this->field_login,
+			$this->field_pass,
+			$this->field_token
+		));
+	}
 
-        function save() /* throw */
-        {
-                $stmt = $this->Database->prepare("SELECT id FROM users WHERE id = ?;");
-                $stmt->execute([$this->id]);
-                if ($stmt->rowCount() > 0)
-                        $this->update();
-                else
-                        $this->insert();
-        }
+	function update() /* throw */
+	{
+		$this->updateQuery("UPDATE users SET login = ?, pass = ?, token = ?, enabled = ? WHERE id = ?;", array(
+			$this->field_login,
+			$this->field_pass,
+			$this->field_token,
+			$this->field_enabled,
+			$this->field_id
+		));
+	}
+
+	function save() /* throw */
+	{
+		$stmt = $this->Database->prepare("SELECT id FROM users WHERE id = ?;");
+		$stmt->execute([$this->id]);
+		if ($stmt->rowCount() > 0)
+			$this->update();
+		else
+			$this->insert();
+	}
 }
--- server/html/pages/user.php
+++ server/html/pages/user.php
@@ -2,18 +2,37 @@
 require_once 'classes/User.php';
 require_once 'scripts/json.php';
 
-function POST()
+function GET()
 {
-	if (!isset($_POST['login'], $_POST['pass']))
+	if (!isset($_GET['login'], $_GET['pass']))
 		die(jsonError("Mandatory parameter not given"));
 	$User = new User();
-	$User->login = $_POST['login'];
-	$User->pass = $_POST['pass'];
+	$User->login = $_GET['login'];
+	$User->pass = $_GET['pass'];
+	try {
+		$User->loadByLogin($User->login);
+	}
+	catch (PDOException $e) {
+		die(jsonError("SQL Error: " . $e->getMessage()));
+	}
+	catch (Exception $e) {
+		die(jsonError("Error: " . $e->getMessage()));
+	}
+	echo jsonMsg("token", $User->token);
+}
+
+function POST()
+{
+	if (!isset($_GET['login'], $_GET['pass']))
+		die(jsonError("Mandatory parameter not given"));
+	$User = new User();
+	$User->login = $_GET['login'];
+	$User->pass = $_GET['pass'];
 	try {
 		$User->insert();
 	}
 	catch (PDOException $e) {
-		die (jsonError("SQL Error: " . $e->getMessage()));
+		die(jsonError("SQL Error: " . $e->getMessage()));
 	}
-	echo jsonMsg();
+	echo jsonMsg("token", $User->token);
 }
