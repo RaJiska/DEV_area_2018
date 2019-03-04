837d57203762fc40b060e7d7cbe838f355ce9b2f
drping <cedric.ogire@epitech.eu>
2019-03-04 18:10:52 +0100

    feat(mobile): login and register in kotlin

--- mobile/DEV_area_2018/app/src/main/java/com/example/dev_area_2018/Register.kt
+++ mobile/DEV_area_2018/app/src/main/java/com/example/dev_area_2018/Register.kt
@@ -1,45 +1,44 @@
 package com.example.dev_area_2018
 
 import android.animation.Animator
-import android.content.Context
 import android.content.Intent
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
-import android.view.View.VISIBLE
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONArray
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import com.android.volley.VolleyError
 import java.lang.Exception
-import java.net.HttpURLConnection
-import java.net.URL
 
-class MainActivity : AppCompatActivity() {
+
+class Register : AppCompatActivity() {
 
     var token = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        setContentView(R.layout.activity_main)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_register)
         object : CountDownTimer(1000, 1000) {
             override fun onFinish() {
                 bookITextView.visibility = View.GONE
                 loadingProgressBar.visibility = View.GONE
-                rootView.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.colorSplashText))
+                rootView.setBackgroundColor(ContextCompat.getColor(this@Register, R.color.colorSplashText))
                 bookIconImageView.setImageResource(R.drawable.background_color_book)
                 startAnimation()
             }
@@ -59,7 +58,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onAnimationEnd(p0: Animator?) {
-                afterAnimationView.visibility = VISIBLE
+                afterAnimationView.visibility = View.VISIBLE
             }
 
             override fun onAnimationCancel(p0: Animator?) {
@@ -72,14 +71,13 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    fun onClickLogIn(v: View) {
+    fun onClickRegister(v: View) {
         val login = findViewById<EditText>(R.id.emailEditText).text
         val pass = findViewById<EditText>(R.id.passwordEditText).text
         val queue = Volley.newRequestQueue(this)
-        val url = "http://area-server.rajiska.fr/user?login=$login&pass=$pass"
-        val stringReq = StringRequest(
-            Request.Method.GET, url,
-            Response.Listener<String> { response ->
+        var url = "http://area-server.rajiska.fr/user"
+        val postRequest = object : StringRequest(Request.Method.POST, url,
+            Response.Listener { response ->
                 var strResp = response.toString()
                 try {
                     val obj = JSONObject(strResp)
@@ -89,13 +87,25 @@ class MainActivity : AppCompatActivity() {
                     println(e)
                 }
             },
-            Response.ErrorListener { println("[ERROR]: FAILED") })
-        queue.add(stringReq)
+            Response.ErrorListener { response ->
+                Log.d("Error.Response", response.toString())
+            }
+        ) {
+            override fun getParams(): Map<String, String> {
+                val params = HashMap<String, String>()
+                params["login"] = login.toString()
+                params["pass"] = pass.toString()
+
+                return params
+            }
+        }
+        queue.add(postRequest)
     }
 
-    fun onCLickRegisterActivity(v: View) {
-        val intent = Intent(this, Register::class.java)
+    fun onClickSignInActivity(v: View) {
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
-}
 
+
+}
