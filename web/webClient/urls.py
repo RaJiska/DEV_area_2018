9389a2260fa5e55de56649a1ae4d1b3fafd0bd3d
kevsev <kevin.severin@epitech.eu>
2019-03-08 16:31:41 +0100

    feat(services): Twitter, Github, Imgur auth

--- web/webClient/urls.py
+++ web/webClient/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url('home', views.home, name='home'),
     url('login', views.loginview, name='login'),
     url('signup', views.signup, name='signup'),
-    url('profile', views.profile),
+    url('profile', views.profile, name='profile'),
     url('imgur', views.imgur),
     url('services', views.services),
     url('connexion', views.connexion),
